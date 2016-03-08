70bc8e675283b714ae3f2ba210008339c4e48725
Ian Alejandro <ialejand@udel.edu>
2016-03-08 00:58:20 -0500

    Init commit

---
+++ Quiz 3/src/GeometricObject.java
@@ -0,0 +1,27 @@
+public abstract class GeometricObject {
+  private String color = "white";
+  private boolean filled;
+  private java.util.Date dateCreated;
+
+  /** Construct a default geometric object */
+  protected GeometricObject() {
+    dateCreated = new java.util.Date();
+  }
+
+  /** Get dateCreated */
+  public java.util.Date getDateCreated() {
+    return dateCreated;
+  }
+
+  @Override
+  public String toString() {
+    return "created on " + dateCreated + "\ncolor: " + color +
+      " and filled: " + filled;
+  }
+
+  /** Abstract method getArea */
+  public abstract double getArea();
+
+  /** Abstract method getPerimeter */
+  public abstract double getPerimeter();
+}
---
+++ Quiz 3/src/TriangleTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TriangleTest  {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three sides of the Triangle");
+        double side1 = input.nextDouble();
+        double side2 = input.nextDouble();
+        double side3 = input.nextDouble();
+        Triangle triangle = new Triangle(side1, side2, side3);
+        System.out.println("The Triangle Sides are \n side 1: "+ triangle.getSide1() + "\n Side 2: " + triangle.getSide2() + "\n Side 3: " + triangle.getSide3());
+        System.out.println("The Triangle's Area is " + triangle.getArea());
+        System.out.println("The Triangle's Perimeter is " + triangle.getPerimeter());
+
+        input.close();
+    }
+}
